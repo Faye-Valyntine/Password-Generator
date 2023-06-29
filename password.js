44df72e6d6a1c04dc64d825020fc8b122efc38a6
Faye-Valyntine <138057345+Faye-Valyntine@users.noreply.github.com>
2023-06-29 16:04:22 -0500

    Add files via upload

---
+++ password.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const getRandomNumber = max => {
+	let random = null;
+	if (!isNaN(max)) {
+		random = Math.random();             // value >= 0.0 and < 1.0
+		random = Math.floor(random * max);  // value is an integer between 0 and max - 1
+		random = random;                // value is an integer between 1 and max
+	}
+	return random;
+};
+
+$(document).ready( () => {
+    $("#generate").click( () => {
+        $("#password").val( "" ); // clear previous entry
+
+        let pass = "";
+
+        const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
+
+        let num = $("#num").val().trim();
+    
+        if (isNaN(num)) {
+            alert("Please enter a valid number")
+        }
+        else {
+            for (let i = 0; i < num; i++){
+                pass += chars[getRandomNumber(chars.length)];
+            } 
+            $("#password").val(pass);
+        }
+
+        
+    }); // end click()
+    
+    $("#clear").click( () => {
+        $("#num").val( "" );
+        $("#password").val( "" );
+        $("#num").focus();
+    }); // end click()
+    
+    // set focus on initial load
+    $("#num").focus();
+}); // end ready()
